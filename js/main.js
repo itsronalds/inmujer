9f3c7866a776f9289c4620c55cd44db243dc7511
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-03-02 20:40:04 -0400

    About page and refactor

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ let isMobileNavOpen = false;
 function mobileNavHandler() {
   if (!isMobileNavOpen) {
     document.querySelector('.header nav').classList.add('open');
+    document.querySelector('.header').classList.remove('border-radius');
 
     document.body.style.overflow = 'hidden';
 
     isMobileNavOpen = true;
   } else {
     document.querySelector('.header nav').classList.remove('open');
+    document.querySelector('.header').classList.add('border-radius');
 
     document.body.style.overflow = 'auto';
 
